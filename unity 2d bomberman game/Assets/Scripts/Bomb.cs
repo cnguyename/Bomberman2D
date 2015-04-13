97e3852efad6e314317d5834ade837a2115444f7
cnguyename <cnguyename@gmail.com>
2015-04-13 09:21:46 -0700

    First commit

---
+++ unity 2d bomberman game/Assets/Scripts/Bomb.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bomb : MonoBehaviour {
+    // Time after which the bomb explodes
+    float time = 3.0f;
+
+    // Explosion Prefab
+    public GameObject explosion;
+
+	public static float count = 1;
+
+	public Vector3 scale;
+
+	public static float scaleX = 0.9F;
+	public static float scaleY = 0.9F;
+
+    void Start () {
+        // Call the Explode function after a few seconds
+        Invoke("Explode", time);
+    }
+    
+    void Explode() {
+        // Remove Bomb from game
+        Destroy(gameObject);
+		BombDrop.bombs_on_field -= 1;
+
+		scale = explosion.transform.localScale;
+
+        // Spawn Explosion
+		if (count == 1) {
+			scale.y = scaleX;
+			scale.x = scaleY;
+			explosion.transform.localScale = scale;
+			Instantiate (explosion,
+                    transform.position,
+                    Quaternion.identity);
+		} else if (count > 1) {
+			scale.y = scaleY * count;
+			scale.x = scaleX * count;
+			explosion.transform.localScale = scale;
+			Instantiate (explosion,
+			                             transform.position,
+			                             Quaternion.identity);
+		}
+
+        // Destroy stuff
+        Vector2 pos = transform.position;
+        Block.destroyBlockAt(pos.x,   pos.y);   // same
+        Block.destroyBlockAt(pos.x,   pos.y+1); // top
+        Block.destroyBlockAt(pos.x+1, pos.y);   // right
+        Block.destroyBlockAt(pos.x,   pos.y-1); // bottom
+        Block.destroyBlockAt(pos.x-1, pos.y);   // left
+        if (count > 1)
+        {
+            Block.destroyBlockAt(pos.x + count, pos.y);
+            Block.destroyBlockAt(pos.x - count, pos.y);
+            Block.destroyBlockAt(pos.x, pos.y + count);
+            Block.destroyBlockAt(pos.x, pos.y - count);
+        }
+    }
+}
---
+++ unity 2d bomberman game/Assets/Scripts/BombPowerup.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombPowerup : MonoBehaviour {
+		
+	void OnCollisionEnter(Collision collisionInfo)
+	{
+		print("Detected collision between " + gameObject.name + " and " + collisionInfo.collider.name);
+		print("There are " + collisionInfo.contacts.Length + " point(s) of contacts");
+		print("Their relative velocity is " + collisionInfo.relativeVelocity);
+	}
+}
